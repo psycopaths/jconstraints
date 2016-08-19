2c161f014f541091dd097a0517867f0fbce74a6f
mmuesly <mail.mues@gmail.com>
2016-08-19 13:28:05 +0200

    updated licence header

--- src/test/java/gov/nasa/jpf/constraints/solver/ExpressionConversionTest.java
+++ src/test/java/gov/nasa/jpf/constraints/solver/ExpressionConversionTest.java
@@ -1,23 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015, United States Government, as represented by the 
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The PSYCO: A Predicate-based Symbolic Compositional Reasoning environment 
+ * platform is licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
  */
 package gov.nasa.jpf.constraints.solver;
 
-import gov.nasa.jpf.constraints.api.Expression;
 import gov.nasa.jpf.constraints.solvers.nativez3.NativeZ3TojConstraintConverter;
-import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.Before;
 import org.testng.annotations.Test;
 
-/**
- *
- * @author mmuesly
- */
 public class ExpressionConversionTest {
   
   public ExpressionConversionTest() {
@@ -31,14 +32,6 @@ public class ExpressionConversionTest {
   public void testExpressionConversion(){
     String z3ExprString = "(exists ('a':integer, 'b':integer):"
             +" ((('x' == ('a' + 'b')) && ('a' > 0)) && ('b' > 0)))";
-    //System.out.println("gov.nasa.jpf.constraints.solver.ExpressionConversionTest.testExpressionConversion() - convCreation");
     NativeZ3TojConstraintConverter converter = new NativeZ3TojConstraintConverter();
-    //Expression<Boolean> test = converter.parse("(>= x 2)");
-    //System.out.println("gov.nasa.jpf.constraints.solver.ExpressionConversionTest.testExpressionConversion() - expressionResult");
-//    try {
-//      test.print(System.out);
-//    } catch (IOException ex) {
-//      Logger.getLogger(ExpressionConversionTest.class.getName()).log(Level.SEVERE, null, ex);
-//    }
   }
 }
