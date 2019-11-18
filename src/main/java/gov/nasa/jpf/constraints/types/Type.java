774b98faa8645ca7a5106587c0a1a6b69786d99b
Malte Mues <mail.mues@gmail.com>
2019-11-18 19:22:25 +0100

    Detect spurious Double representations

--- src/main/java/gov/nasa/jpf/constraints/types/Type.java
+++ src/main/java/gov/nasa/jpf/constraints/types/Type.java
@@ -1,44 +1,45 @@
 /*
- * Copyright (C) 2015, United States Government, as represented by the 
+ * Copyright (C) 2015, United States Government, as represented by the
  * Administrator of the National Aeronautics and Space Administration.
  * All rights reserved.
  *
- * The PSYCO: A Predicate-based Symbolic Compositional Reasoning environment 
- * platform is licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may obtain a 
- * copy of the License at http://www.apache.org/licenses/LICENSE-2.0. 
+ * The PSYCO: A Predicate-based Symbolic Compositional Reasoning environment
+ * platform is licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Unless required by applicable law or agreed to in writing, software distributed 
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
 
 package gov.nasa.jpf.constraints.types;
 
 import gov.nasa.jpf.constraints.casts.CastOperation;
+import gov.nasa.jpf.constraints.exceptions.ImpreciseRepresentationException;
 
 public interface Type<T> {
 
-  public String getName();
-  
-  public String[] getOtherNames();
-  
-  public Class<T> getCanonicalClass();
+	public String getName();
 
-  public Class<?>[] getOtherClasses();
+	public String[] getOtherNames();
 
-  public T cast(Object other);
+	public Class<T> getCanonicalClass();
 
-  public T getDefaultValue();
+	public Class<?>[] getOtherClasses();
 
-  public Type<?> getSuperType();
+	public T cast(Object other);
 
-  public <O> CastOperation<? super O,? extends T> cast(Type<O> fromType);
-  public <O> CastOperation<? super O,? extends T> requireCast(Type<O> fromType);
+	public T getDefaultValue();
 
-  public abstract T parse(String string);
+	public Type<?> getSuperType();
 
-  
-	
+	public <O> CastOperation<? super O, ? extends T> cast(Type<O> fromType);
+
+	public <O> CastOperation<? super O, ? extends T> requireCast(Type<O> fromType);
+
+	public abstract T parse(String string) throws ImpreciseRepresentationException;
+
+
 }
