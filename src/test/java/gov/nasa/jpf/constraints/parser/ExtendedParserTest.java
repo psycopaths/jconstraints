4a0b6184ca95b83a437488f36d1c4239a329e64f
Malte Mues <mail.mues@gmail.com>
2019-11-27 10:39:31 +0100

    Fix broken exception handling in tests

--- src/test/java/gov/nasa/jpf/constraints/parser/ExtendedParserTest.java
+++ src/test/java/gov/nasa/jpf/constraints/parser/ExtendedParserTest.java
@@ -7,6 +7,7 @@ package gov.nasa.jpf.constraints.parser;
 
 import gov.nasa.jpf.constraints.api.Expression;
 import gov.nasa.jpf.constraints.api.Variable;
+import gov.nasa.jpf.constraints.exceptions.ImpreciseRepresentationException;
 import gov.nasa.jpf.constraints.expressions.Constant;
 import gov.nasa.jpf.constraints.expressions.LogicalOperator;
 import gov.nasa.jpf.constraints.expressions.NumericBooleanExpression;
@@ -18,177 +19,181 @@ import gov.nasa.jpf.constraints.expressions.Quantifier;
 import gov.nasa.jpf.constraints.expressions.QuantifierExpression;
 import gov.nasa.jpf.constraints.types.BuiltinTypes;
 import gov.nasa.jpf.constraints.types.TypeContext;
-import java.util.HashSet;
-import java.util.List;
 import org.antlr.runtime.RecognitionException;
-import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 /**
- * We just want to assure that a certain set 
- * of constraints is parsable without errors
+ * We just want to assure that a certain set of constraints is parsable without errors
+ *
  * @author Malte Mues
  */
 public class ExtendedParserTest {
-  ParserUtil parser;
-  
-  Variable x, b, c;
-  HashSet<Variable<?>> vars;
-  public ExtendedParserTest() {
-    parser = new ParserUtil();
-    x = new Variable(BuiltinTypes.SINT8, "x");
-    b = new Variable(BuiltinTypes.BOOL, "b");
-    c = new Variable(BuiltinTypes.SINT32, "c");
-    vars = new HashSet<>();
-    vars.add(x);
-    vars.add(c);
-    vars.add(b);
-  }
-  
+	ParserUtil parser;
 
-  @Test
-  public void variableDeclarationOfPrimeVariables() throws RecognitionException {
-        String varDeclaration = "declare x:sint8, b:bool, c:sint32";
-        String primeVarDeclaration = "declare x':sint8, b':bool, c':sint32";
-        
-    List<Variable<?>> parsedVar = parser.parseVariableDeclaration(varDeclaration);
-    parsedVar.addAll(parser.parseVariableDeclaration(primeVarDeclaration));
-    assert(parsedVar.contains(x));
-    assert(parsedVar.contains(b));
-    assert(parsedVar.contains(c));
-    
-    Variable xprime, bprime, cprime;
-    xprime = new Variable(BuiltinTypes.SINT8, "x'");
-    bprime = new Variable(BuiltinTypes.BOOL, "b'");
-    cprime = new Variable(BuiltinTypes.SINT32, "c'");
-    assert(parsedVar.contains(xprime));
-    assert(parsedVar.contains(bprime));
-    assert(parsedVar.contains(cprime));
-  }
+	Variable x, b, c;
+	HashSet<Variable<?>> vars;
 
-  @Test
-  public void usingPrimeVariables() throws RecognitionException {
-    String testInput = "declare x:sint32, x':sint32 in x > 5 && x' == 5";
-    Expression parsedRes = parser.parseLogical(testInput);
-    assertEquals(parsedRes.getClass(), PropositionalCompound.class);
-    
-    testInput = "declare x':sint32 in forall (x') : (x' > 5b && (exists (c) : (c > x)))";
-    parsedRes = parser.parseLogical(testInput, new TypeContext(true), vars);
-    
-    assertEquals(QuantifierExpression.class, parsedRes.getClass());
-  }
+	public ExtendedParserTest() {
+		parser = new ParserUtil();
+		x = new Variable(BuiltinTypes.SINT8, "x");
+		b = new Variable(BuiltinTypes.BOOL, "b");
+		c = new Variable(BuiltinTypes.SINT32, "c");
+		vars = new HashSet<>();
+		vars.add(x);
+		vars.add(c);
+		vars.add(b);
+	}
 
-  @Test
-  public void variableDeclaration() throws RecognitionException {
-    String varDeclaration ="declare x:sint8, b:bool, c:sint32";
-    
-    List<Variable<?>> parsedVar = parser.parseVariableDeclaration(varDeclaration);
-    
-    assert(parsedVar.contains(x));
-    assert(parsedVar.contains(b));
-    assert(parsedVar.contains(c));
-  }
-  
-  @Test
-  public void andBooleanExpression() throws RecognitionException {
-    String testString = "declare x:sint8, b:bool, c:sint32 in (c == 5) && (b == false) && (x > c)";
-    Expression expr = parser.parseLogical(testString);
-    
-    assertTrue(checkAndExpression(expr));
-    
-    testString = "declare x:sint8, b:bool, c:sint32 in c == 5 && b == false && x > c";
-    expr = parser.parseLogical(testString);
-    
-    assertTrue(checkAndExpression(expr));
-  }
-  
-  @Test
-  public void orBooleanExpression() throws RecognitionException {
-    //the 5b forces the parser to interpret 5 of type sint8. Otherwise an
-    //undesired castexpression is added...
-    String testString = "x + 5b > c || b == false";
-    Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
-    
-    assertEquals(PropositionalCompound.class, expr.getClass());
-    PropositionalCompound propCompound = (PropositionalCompound) expr;
-    assertEquals(LogicalOperator.OR, propCompound.getOperator());
-    
-    NumericBooleanExpression assignmentB = (NumericBooleanExpression) propCompound.getRight();
-    assertEquals(b, assignmentB.getLeft());
-    assertEquals(NumericComparator.EQ, assignmentB.getComparator());
-    
-    assertEquals(NumericBooleanExpression.class, propCompound.getLeft().getClass());
-    NumericBooleanExpression comparisonC = (NumericBooleanExpression) propCompound.getLeft();
-    assertEquals(NumericComparator.GT, comparisonC.getComparator());
-    assertEquals(c, comparisonC.getRight());
-    
-    assertEquals(NumericCompound.class, comparisonC.getLeft().getClass());
-    NumericCompound additionX = (NumericCompound) comparisonC.getLeft();
 
-    Variable parsedX = (Variable) additionX.getLeft();
+	@Test
+	public void variableDeclarationOfPrimeVariables() throws RecognitionException, ImpreciseRepresentationException {
+		String varDeclaration = "declare x:sint8, b:bool, c:sint32";
+		String primeVarDeclaration = "declare x':sint8, b':bool, c':sint32";
 
-    assertEquals(x.getType(), parsedX.getType());
-    assertEquals(x.getName(), parsedX.getName());
-    assertEquals(x, additionX.getLeft());
-    assertEquals(Constant.class, additionX.getRight().getClass());
-    assertEquals(NumericOperator.PLUS, additionX.getOperator());
-  }
-  
-  private boolean checkAndExpression(Expression<Boolean> expr){
-    assertEquals(PropositionalCompound.class, expr.getClass());
-    PropositionalCompound propCompound = (PropositionalCompound) expr;
-    assertEquals(NumericBooleanExpression.class, propCompound.getRight().getClass());
-    assertEquals(LogicalOperator.AND, propCompound.getOperator());
-    assertEquals(PropositionalCompound.class, propCompound.getLeft().getClass());
-    
-    NumericBooleanExpression comparisonXC = (NumericBooleanExpression) propCompound.getRight();
-    propCompound = (PropositionalCompound) propCompound.getLeft();
-    
-    assertEquals(x, comparisonXC.getLeft());
-    assertEquals(c, comparisonXC.getRight());
-    assertEquals(NumericComparator.GT, comparisonXC.getComparator());
-    
-    NumericBooleanExpression assignmentC = (NumericBooleanExpression) propCompound.getLeft();
-    assertEquals(c, assignmentC.getLeft());
-    assertEquals(NumericComparator.EQ, assignmentC.getComparator());
-    
-    NumericBooleanExpression assignmentB = (NumericBooleanExpression) propCompound.getRight();
-    assertEquals(b, assignmentB.getLeft());
-    assertEquals(NumericComparator.EQ, assignmentB.getComparator());
-    return true;
-  }
-  
-  @Test
-  public void quantifierExpression() throws RecognitionException {
-    String testString = "forall (x) : (x > 5b && (exists (c) : (c > x)))";
-    
-    Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
-    
-    assertEquals(QuantifierExpression.class, expr.getClass());
-    QuantifierExpression allQuantifiedExpr = (QuantifierExpression) expr;
-    assertEquals(Quantifier.FORALL,allQuantifiedExpr.getQuantifier());
-    assertEquals(1, allQuantifiedExpr.getBoundVariables().size());
-    assertEquals(x, allQuantifiedExpr.getBoundVariables().get(0));
-    
-    assertEquals(PropositionalCompound.class, allQuantifiedExpr.getBody().getClass());
-    PropositionalCompound propCompound = (PropositionalCompound) allQuantifiedExpr.getBody();
-    
-    assertEquals(QuantifierExpression.class, propCompound.getRight().getClass());
-    QuantifierExpression existQuantifiedExpr = (QuantifierExpression) propCompound.getRight();
-    assertEquals(Quantifier.EXISTS, existQuantifiedExpr.getQuantifier());
-    assertEquals(1, existQuantifiedExpr.getBoundVariables().size());
-    assertEquals(c, existQuantifiedExpr.getBoundVariables().get(0));
-  }
-  
-  @Test(expectedExceptions = {UndeclaredVariableException.class})
-  public void undeclaredVarInQuantifiedExpression() throws RecognitionException {
-    String testString = "forall (y:sint32) : (exists (a) : (x > 5b && c > 3))";
-    Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
-  }
-  
-  @Test(expectedExceptions = {RecognitionException.class})
-  public void wrongEQ() throws RecognitionException{
-    String testString = "declare x:sint32 in x = 5";
-    Expression expr = parser.parseLogical(testString);
-  }
+		List<Variable<?>> parsedVar = parser.parseVariableDeclaration(varDeclaration);
+		parsedVar.addAll(parser.parseVariableDeclaration(primeVarDeclaration));
+		assert (parsedVar.contains(x));
+		assert (parsedVar.contains(b));
+		assert (parsedVar.contains(c));
+
+		Variable xprime, bprime, cprime;
+		xprime = new Variable(BuiltinTypes.SINT8, "x'");
+		bprime = new Variable(BuiltinTypes.BOOL, "b'");
+		cprime = new Variable(BuiltinTypes.SINT32, "c'");
+		assert (parsedVar.contains(xprime));
+		assert (parsedVar.contains(bprime));
+		assert (parsedVar.contains(cprime));
+	}
+
+	@Test
+	public void usingPrimeVariables() throws RecognitionException, ImpreciseRepresentationException {
+		String testInput = "declare x:sint32, x':sint32 in x > 5 && x' == 5";
+		Expression parsedRes = parser.parseLogical(testInput);
+		assertEquals(parsedRes.getClass(), PropositionalCompound.class);
+
+		testInput = "declare x':sint32 in forall (x') : (x' > 5b && (exists (c) : (c > x)))";
+		parsedRes = parser.parseLogical(testInput, new TypeContext(true), vars);
+
+		assertEquals(QuantifierExpression.class, parsedRes.getClass());
+	}
+
+	@Test
+	public void variableDeclaration() throws RecognitionException, ImpreciseRepresentationException {
+		String varDeclaration = "declare x:sint8, b:bool, c:sint32";
+
+		List<Variable<?>> parsedVar = parser.parseVariableDeclaration(varDeclaration);
+
+		assert (parsedVar.contains(x));
+		assert (parsedVar.contains(b));
+		assert (parsedVar.contains(c));
+	}
+
+	@Test
+	public void andBooleanExpression() throws RecognitionException, ImpreciseRepresentationException {
+		String testString = "declare x:sint8, b:bool, c:sint32 in (c == 5) && (b == false) && (x > c)";
+		Expression expr = parser.parseLogical(testString);
+
+		assertTrue(checkAndExpression(expr));
+
+		testString = "declare x:sint8, b:bool, c:sint32 in c == 5 && b == false && x > c";
+		expr = parser.parseLogical(testString);
+
+		assertTrue(checkAndExpression(expr));
+	}
+
+	@Test
+	public void orBooleanExpression() throws RecognitionException, ImpreciseRepresentationException {
+		//the 5b forces the parser to interpret 5 of type sint8. Otherwise an
+		//undesired castexpression is added...
+		String testString = "x + 5b > c || b == false";
+		Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
+
+		assertEquals(PropositionalCompound.class, expr.getClass());
+		PropositionalCompound propCompound = (PropositionalCompound) expr;
+		assertEquals(LogicalOperator.OR, propCompound.getOperator());
+
+		NumericBooleanExpression assignmentB = (NumericBooleanExpression) propCompound.getRight();
+		assertEquals(b, assignmentB.getLeft());
+		assertEquals(NumericComparator.EQ, assignmentB.getComparator());
+
+		assertEquals(NumericBooleanExpression.class, propCompound.getLeft().getClass());
+		NumericBooleanExpression comparisonC = (NumericBooleanExpression) propCompound.getLeft();
+		assertEquals(NumericComparator.GT, comparisonC.getComparator());
+		assertEquals(c, comparisonC.getRight());
+
+		assertEquals(NumericCompound.class, comparisonC.getLeft().getClass());
+		NumericCompound additionX = (NumericCompound) comparisonC.getLeft();
+
+		Variable parsedX = (Variable) additionX.getLeft();
+
+		assertEquals(x.getType(), parsedX.getType());
+		assertEquals(x.getName(), parsedX.getName());
+		assertEquals(x, additionX.getLeft());
+		assertEquals(Constant.class, additionX.getRight().getClass());
+		assertEquals(NumericOperator.PLUS, additionX.getOperator());
+	}
+
+	private boolean checkAndExpression(Expression<Boolean> expr) {
+		assertEquals(PropositionalCompound.class, expr.getClass());
+		PropositionalCompound propCompound = (PropositionalCompound) expr;
+		assertEquals(NumericBooleanExpression.class, propCompound.getRight().getClass());
+		assertEquals(LogicalOperator.AND, propCompound.getOperator());
+		assertEquals(PropositionalCompound.class, propCompound.getLeft().getClass());
+
+		NumericBooleanExpression comparisonXC = (NumericBooleanExpression) propCompound.getRight();
+		propCompound = (PropositionalCompound) propCompound.getLeft();
+
+		assertEquals(x, comparisonXC.getLeft());
+		assertEquals(c, comparisonXC.getRight());
+		assertEquals(NumericComparator.GT, comparisonXC.getComparator());
+
+		NumericBooleanExpression assignmentC = (NumericBooleanExpression) propCompound.getLeft();
+		assertEquals(c, assignmentC.getLeft());
+		assertEquals(NumericComparator.EQ, assignmentC.getComparator());
+
+		NumericBooleanExpression assignmentB = (NumericBooleanExpression) propCompound.getRight();
+		assertEquals(b, assignmentB.getLeft());
+		assertEquals(NumericComparator.EQ, assignmentB.getComparator());
+		return true;
+	}
+
+	@Test
+	public void quantifierExpression() throws RecognitionException, ImpreciseRepresentationException {
+		String testString = "forall (x) : (x > 5b && (exists (c) : (c > x)))";
+
+		Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
+
+		assertEquals(QuantifierExpression.class, expr.getClass());
+		QuantifierExpression allQuantifiedExpr = (QuantifierExpression) expr;
+		assertEquals(Quantifier.FORALL, allQuantifiedExpr.getQuantifier());
+		assertEquals(1, allQuantifiedExpr.getBoundVariables().size());
+		assertEquals(x, allQuantifiedExpr.getBoundVariables().get(0));
+
+		assertEquals(PropositionalCompound.class, allQuantifiedExpr.getBody().getClass());
+		PropositionalCompound propCompound = (PropositionalCompound) allQuantifiedExpr.getBody();
+
+		assertEquals(QuantifierExpression.class, propCompound.getRight().getClass());
+		QuantifierExpression existQuantifiedExpr = (QuantifierExpression) propCompound.getRight();
+		assertEquals(Quantifier.EXISTS, existQuantifiedExpr.getQuantifier());
+		assertEquals(1, existQuantifiedExpr.getBoundVariables().size());
+		assertEquals(c, existQuantifiedExpr.getBoundVariables().get(0));
+	}
+
+	@Test(expectedExceptions = {UndeclaredVariableException.class})
+	public void undeclaredVarInQuantifiedExpression() throws RecognitionException, ImpreciseRepresentationException {
+		String testString = "forall (y:sint32) : (exists (a) : (x > 5b && c > 3))";
+		Expression expr = parser.parseLogical(testString, new TypeContext(true), vars);
+	}
+
+	@Test(expectedExceptions = {RecognitionException.class})
+	public void wrongEQ() throws RecognitionException, ImpreciseRepresentationException {
+		String testString = "declare x:sint32 in x = 5";
+		Expression expr = parser.parseLogical(testString);
+	}
 }
