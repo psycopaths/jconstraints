31235de4be2c309541285f208885577fbfd2b85a
Malte Mues <mail.mues@gmail.com>
2020-06-12 22:51:31 +0200

    debugging

--- src/test/java/io/github/tudoaqua/jconstraints/cvc4/CVC4SolverTest.java
+++ src/test/java/io/github/tudoaqua/jconstraints/cvc4/CVC4SolverTest.java
@@ -12,7 +12,64 @@
  */
 package io.github.tudoaqua.jconstraints.cvc4;
 
+import edu.nyu.acsys.CVC4.CVC4JNI;
+import edu.nyu.acsys.CVC4.Expr;
+import edu.nyu.acsys.CVC4.ExprManager;
+import edu.nyu.acsys.CVC4.Kind;
+import edu.nyu.acsys.CVC4.Rational;
+import edu.nyu.acsys.CVC4.Result;
+import edu.nyu.acsys.CVC4.Result.Sat;
+import edu.nyu.acsys.CVC4.SmtEngine;
+import edu.nyu.acsys.CVC4.Type;
 import org.testng.annotations.Test;
 
+import static edu.nyu.acsys.CVC4.Result.*;
+import static org.testng.Assert.assertEquals;
+
 public class CVC4SolverTest {
-	@Test }
+	@Test
+	public void firstTest() {
+		ExprManager em = new ExprManager();
+		SmtEngine smt = new SmtEngine(em);
+
+		smt.setLogic("QF_LIRA"); // Set the logic
+
+		// Types
+		Type real = em.realType();
+		Type integer = em.integerType();
+
+		// Variables
+		Expr x = em.mkVar("x", integer);
+		Expr y = em.mkVar("a", real);
+		Expr b = em.mkVar("b", integer);
+
+		// Constants
+		Expr three = em.mkConst(new Rational(3));
+		Expr nullC = em.mkConst(new Rational(0));
+		Expr two_thirds = em.mkConst(new Rational(2, 3));
+
+		// Terms
+		Expr plus = em.mkExpr(Kind.PLUS, y, b);
+		Expr diff = em.mkExpr(Kind.MINUS, y, x);
+
+		// Formulas
+		Expr x_geq_3y = em.mkExpr(Kind.EQUAL, x, plus);
+		Expr x_leq_y = em.mkExpr(Kind.GT, y, nullC);
+		Expr neg2_lt_x = em.mkExpr(Kind.GT, b, nullC);
+
+		Expr assumptions = em.mkExpr(Kind.AND, x_geq_3y, x_leq_y);
+		assumptions = em.mkExpr(Kind.AND, assumptions, neg2_lt_x);
+		smt.assertFormula(assumptions);
+
+		smt.push();
+		Expr diff_leq_two_thirds = em.mkExpr(Kind.LEQ, diff, two_thirds);
+		assertEquals(smt.query(diff_leq_two_thirds).toString().toLowerCase(), Validity.VALID.toString().toLowerCase());
+
+		smt.pop();
+
+		smt.push();
+
+		assertEquals(smt.checkSat(em.mkConst(true)).toString().toLowerCase(), Sat.SAT.toString().toLowerCase());
+		smt.pop();
+	}
+}
