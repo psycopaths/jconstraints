297246df3f28c9ad7163dcb351b528b4fec5235b
Malte Mues <mail.mues@gmail.com>
2020-10-23 01:04:23 +0200

    Add support for exporting to configured file

--- src/main/java/gov/nasa/jpf/constraints/smtlibUtility/solver/SMTLibExportGenContext.java
+++ src/main/java/gov/nasa/jpf/constraints/smtlibUtility/solver/SMTLibExportGenContext.java
@@ -111,6 +111,7 @@ public class SMTLibExportGenContext {
 
 	public void solve() {
 		out.println("(check-sat)");
+		out.flush();
 	}
 
 	public void flush() {
--- src/main/java/gov/nasa/jpf/constraints/smtlibUtility/solver/SMTLibExportSolverProvider.java
+++ src/main/java/gov/nasa/jpf/constraints/smtlibUtility/solver/SMTLibExportSolverProvider.java
@@ -4,21 +4,37 @@ import gov.nasa.jpf.constraints.api.ConstraintSolver;
 import gov.nasa.jpf.constraints.solvers.ConstraintSolverFactory;
 import gov.nasa.jpf.constraints.solvers.ConstraintSolverProvider;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Properties;
 
 public class SMTLibExportSolverProvider implements ConstraintSolverProvider {
 
     @Override
     public String[] getNames() {
-        return new String[] { SMTLibExportWrapper.NAME };
+        return new String[]{SMTLibExportWrapper.NAME};
     }
 
     @Override
     public ConstraintSolver createSolver(Properties config) {
-        String backName = config.getProperty( SMTLibExportWrapper.NAME + ".back");
+        String backName = config.getProperty(SMTLibExportWrapper.NAME + ".back");
+        String resultFile = config.getProperty(SMTLibExportWrapper.NAME + ".resultFile", null);
         ConstraintSolverFactory f = new ConstraintSolverFactory();
         ConstraintSolver back = f.createSolver(backName);
-        ConstraintSolver smtWrapper = new SMTLibExportWrapper( back, System.out );
+        PrintStream out = System.out;
+        if (resultFile != null) {
+            File outfile = new File(resultFile);
+            outfile.getAbsoluteFile().getParentFile().mkdirs();
+            try {
+                out = new PrintStream(outfile);
+            }
+            catch (FileNotFoundException e) {
+                System.err.println("Cannot write to: " + resultFile);
+                out = System.out;
+            }
+        }
+        ConstraintSolver smtWrapper = new SMTLibExportWrapper(back, out);
         return smtWrapper;
     }
 }
