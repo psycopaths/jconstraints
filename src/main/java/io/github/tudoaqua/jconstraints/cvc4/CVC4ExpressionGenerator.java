5df1978d811d286d4e00b5640722ff0aeaa7a1a4
Malte Mues <mail.mues@gmail.com>
2020-12-01 17:34:46 +0100

    Fix some bugs discoverd during SV-COMP21

--- src/main/java/io/github/tudoaqua/jconstraints/cvc4/CVC4ExpressionGenerator.java
+++ src/main/java/io/github/tudoaqua/jconstraints/cvc4/CVC4ExpressionGenerator.java
@@ -1,14 +1,16 @@
 /**
  * Copyright 2020 TU Dortmund, Nils Schmidt und Malte Mues
  *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package io.github.tudoaqua.jconstraints.cvc4;
@@ -218,6 +220,13 @@ public class CVC4ExpressionGenerator extends AbstractExpressionVisitor<Expr, Exp
         break;
       case XOR:
         all = em.mkExpr(Kind.XOR, left, right);
+        break;
+      case EQUIV:
+        all = em.mkExpr(Kind.EQUAL, left, right);
+        break;
+      case IMPLY:
+        all = em.mkExpr(Kind.IMPLIES, left, right);
+        break;
       default:
         throw new UnsupportedOperationException("Cannot convert operator: " + n.toString());
     }
@@ -229,6 +238,8 @@ public class CVC4ExpressionGenerator extends AbstractExpressionVisitor<Expr, Exp
     Expr negated = visit(n.getNegated());
     if (n.getNegated().getType() instanceof ConcreteBVIntegerType) {
       return em.mkExpr(Kind.BITVECTOR_NEG, negated);
+    } else if (n.getNegated().getType() instanceof ConcreteFloatingPointType) {
+      return em.mkExpr(Kind.FLOATINGPOINT_NEG, negated);
     } else {
       return em.mkExpr(Kind.UMINUS, negated);
     }
--- src/test/java/io/github/tudoaqua/jconstraints/cvc4/expressions/NumericFPTest.java
+++ src/test/java/io/github/tudoaqua/jconstraints/cvc4/expressions/NumericFPTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 TU Dortmund, Nils Schmidt und Malte Mues
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.tudoaqua.jconstraints.cvc4.expressions;
 
 import static gov.nasa.jpf.constraints.expressions.NumericComparator.EQ;
@@ -18,6 +33,7 @@ import gov.nasa.jpf.constraints.expressions.NumericBooleanExpression;
 import gov.nasa.jpf.constraints.expressions.NumericComparator;
 import gov.nasa.jpf.constraints.expressions.NumericCompound;
 import gov.nasa.jpf.constraints.expressions.NumericOperator;
+import gov.nasa.jpf.constraints.expressions.UnaryMinus;
 import gov.nasa.jpf.constraints.types.BuiltinTypes;
 import io.github.tudoaqua.jconstraints.cvc4.CVC4Solver;
 import java.util.HashMap;
@@ -348,5 +364,18 @@ public class NumericFPTest {
     ConstraintSolver.Result res = cvc4.solve(expr, val);
     assertEquals(res, ConstraintSolver.Result.SAT);
     assertTrue(expr.evaluate(val));
+  }
+
+  @Test
+  public void unaryMinusTest() {
+    Variable var = Variable.create(BuiltinTypes.FLOAT, "x");
+    UnaryMinus um = UnaryMinus.create(var);
+    NumericBooleanExpression nbe =
+        NumericBooleanExpression.create(um, GT, Constant.create(BuiltinTypes.FLOAT, 5.0f));
+
+    Valuation val = new Valuation();
+    ConstraintSolver.Result res = cvc4.solve(nbe, val);
+    assertEquals(res, ConstraintSolver.Result.SAT);
+    assertTrue(nbe.evaluate(val));
   }
 }
