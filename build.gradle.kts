6b96b09a731014e995705e812d3be4914670a7c1
Malte Mues <mail.mues@gmail.com>
2021-03-03 10:12:04 +0100

    Make jconstraints-z3 publish to maven local again

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,13 @@ repositories {
 
 group = "tools.aqua"
 version = "0.9.6-SNAPSHOT"
-description = "jConstraints-z3"
+description = "JConstraints-z3"
 
 dependencies {
     implementation("io.github.tudo-aqua:z3-turnkey:4.8.10")
     implementation("tools.aqua:jconstraints-all:0.9.6-SNAPSHOT")
 
     testImplementation("org.testng:testng:7.0.0")
-
 }
 
 java {
@@ -47,10 +46,55 @@ val test by tasks.getting(Test::class) {
 
 tasks.shadowJar {
     //FIXME: Exclude the other jconstraints-deps
-    exclude("tools.aqua:jconstraints-all:.*")
+    exclude("tools.aqua:jconstraints:.*")
+    archiveFileName.set(
+        "${archiveBaseName.get()}-${archiveClassifier.get()}-${archiveVersion.get()}.${archiveExtension.get()}"
+    )
 }
 
 license {
     header = rootProject.file("NOTICE")
     strictCheck = true
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "jconstraints-z3"
+            from(components["java"])
+            pom {
+                name.set("jConstraints-z3")
+                description.set("JConstraints-Z3 is the Z3 API plug-in for JConstraints.")
+                url.set("https://github.com/tudo-aqua/jconstraints-z3")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mmuesly")
+                        name.set("Malte Mues")
+                        email.set("mail.mues@gmail.com")
+                    }
+                    developer {
+                        id.set("fhowar")
+                        name.set("Falk Howar")
+                    }
+                }
+                scm {
+                    connection.set("https://github.com/tudo-aqua/jconstraints-z3")
+                    url.set("https://github.com/tudo-aqua/jconstraints-z3")
+                }
+            }
+        }
+        create<MavenPublication>("publishMaven") {
+            artifact(tasks["shadowJar"]) {
+                classifier = null
+            }
+            artifactId = "jconstraints-z3-all"
+
+        }
+    }
 }
