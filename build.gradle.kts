54afc4ba2ebf323795bb178c1ecfe1f7df414912
Malte Mues <mail.mues@gmail.com>
2021-03-03 15:56:58 +0100

    Update the license management to match JConstraints standard

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,34 @@
 /*
- * Copyright 2020 TU Dortmund, Malte Mues
+ * Copyright 2015 United States Government, as represented by the Administrator
+ *                of the National Aeronautics and Space Administration. All Rights Reserved.
+ *           2017-2021 The jConstraints Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+import java.time.LocalDate.now
+
 plugins {
     `java-library`
     `maven-publish`
-    id("com.github.hierynomus.license") version "0.15.0"
     id("com.github.sherter.google-java-format") version "0.9"
     id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("org.cadixdev.licenser") version "0.5.0"
 }
 
 repositories {
@@ -36,7 +56,7 @@ java {
 }
 
 val test by tasks.getting(Test::class) {
-    useTestNG() {
+    useTestNG {
         useDefaultListeners = true
     }
     testLogging {
@@ -54,8 +74,16 @@ tasks.shadowJar {
 }
 
 license {
-    header = rootProject.file("NOTICE")
-    strictCheck = true
+    header = project.file("contrib/license-header.txt")
+    ext["year"] = now().year
+
+    exclude("**/*.smt2", "**/*.txt")
+
+    tasks {
+        create("buildFiles") {
+            files = project.files("build.gradle.kts", "settings.gradle.kts")
+        }
+    }
 }
 
 publishing {
